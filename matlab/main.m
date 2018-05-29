952134adfabff2eea42f63d1fe30859375f62363
jesusdpinedac@gmail.com <JesusPineda@iMac-de-User.local>
2018-05-29 12:57:09 -0500

    visualize results fig 1 and 2

--- matlab/main.m
+++ matlab/main.m
@@ -2,15 +2,16 @@
 I = double(imread('Data/Image.bmp'));
 
 %% Automatic segmentation
-[cx,cy,bounding_box,area,orientation,fftspectrum] = get_principal_component(I,80,'false');
+[cx,cy,bounding_box,area,orientation,fftspectrum] = get_principal_component(I,80,'true');
 
 %% Objetive function
-s = 2000;
-angl = 20;
+s = 2900;
+angl = 20.4;
 wavelength = 20;
-sigmaOnf = 0.55;
-thetaSigma = 0.25;
+sigmaOnf = 0.6 ;
+thetaSigma = 0.18;
 
-[residue_sum] = funcObj(fftspectrum,cx,cy,wavelength,s,sigmaOnf,thetaSigma,angl,'true');
+[residue_sum,phased,gabor_filter] = funcObj(fftspectrum,cx,cy,wavelength,s,sigmaOnf,thetaSigma,angl,'false');
 
-%% PSO optimization
+figure(1),imagesc(ifftshift(gabor_filter))
+figure(2),imagesc(phased)
